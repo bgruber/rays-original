5cf0836b0aaf2a577d9de607c32992020945ddb1
Brian Gruber <bgruber@meetup.com>
2016-10-20 23:03:24 -0400

    listen for rsvps

--- src/rays_original/core.clj
+++ src/rays_original/core.clj
@@ -1,6 +1,7 @@
 (ns rays-original.core
   (:require [clojure.string :refer [join]]
-            [serial.core :as serial]))
+            [serial.core :as serial]
+            [gniazdo.core :as ws]))
 
 (defn send-command [port command & args]
   (let [command (join " " (cons command args))]
@@ -9,3 +10,18 @@
 (defn blink-led [port n]
   (send-command port "blink" n))
 
+(def rsvp-url "ws://stream.meetup.com/2/rsvps")
+
+(def socket-agent (agent nil))
+(defn listen-rsvps! [handler]
+  (send socket-agent
+        (fn [socket handler]
+          (when (nil? socket)
+            (ws/connect rsvp-url :on-receive handler)))
+        handler))
+(defn unlisten-rsvps! []
+  (send socket-agent
+        (fn [socket]
+          (when-not (nil? socket)
+            (ws/close socket)
+            nil))))
